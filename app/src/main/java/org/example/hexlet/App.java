962b4dbfe8ae8d03b8bb7f0a3df8bd0f5a94a770
Alexey Kiselev <alexey4050@mail.ru>
2025-04-15 14:58:36 +0300

    Added homework HTTP-session

---
+++ app/src/main/java/org/example/hexlet/App.java
@@ -0,0 +1,25 @@
+package org.example.hexlet;
+
+import io.javalin.Javalin;
+
+public class App {
+    public static Javalin getApp() {
+        var app = Javalin.create(config -> {
+            config.bundledPlugins.enableDevLogging();
+        });
+        app.get("/hello", ctx -> {
+            String name = ctx.queryParam("name");
+            if (name == null || name.isEmpty()) {
+                ctx.result("Hello, World");
+            } else {
+                ctx.result("Hello," + name + "!");
+            }
+        });
+        return app;
+    }
+
+    public static void main(String[] args) {
+        Javalin app = getApp();
+        app.start(7070);
+    }
+}
